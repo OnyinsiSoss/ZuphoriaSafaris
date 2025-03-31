e2eb2be44c4d44a3072cd44e78eab7546fba7c84
Onyinsi <onyinsisoss@outlook.com>
2025-03-31 14:08:23 +0300

    .

--- components/sections/features/Features.tsx
+++ components/sections/features/Features.tsx
@@ -17,7 +17,7 @@ export const Features = () => {
           <div className="grid md:grid-cols-3 gap-4">
             <div className="col-span-1 relative h-[600px] rounded-xl overflow-hidden">
               <Image
-                src="/pic-10.jpg"
+                src="pic-10.jpg"
                 alt="Member"
                 width={320} 
                 height={450}
@@ -52,7 +52,7 @@ export const Features = () => {
             </div>
             <div className="col-span-1 relative h-[600px] rounded-xl overflow-hidden">
               <Image
-                src="/pic-11.jpg"
+                src="pic-11.jpg"
                 alt="Member"
                 width={320} 
                 height={450}
@@ -84,7 +84,7 @@ export const Features = () => {
             </div>
             <div className="col-span-1 relative h-[600px] rounded-xl overflow-hidden">
               <Image
-                src="/pic-209.jpg"
+                src="pic-209.jpg"
                 alt="Member"
                 width={320} 
                 height={450}
